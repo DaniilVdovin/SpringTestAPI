b50fdcff38375dc84b02802309dd2f17a73412e6
Dannil Vdovin <stels1040533@gmail.com>
2023-05-29 21:38:56 +0300

    Initial commit

---
+++ src/main/java/com/daniilvdovin/SpringTestAPI/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.daniilvdovin.SpringTestAPI.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String GetHome(){
+        return "Get Home";
+    }
+    @PostMapping("/")
+    public String PostHome(){
+        return "Post Home";
+    }
+    @PutMapping("/")
+    public String PutHome(){
+        return "Put Home";
+    }
+    @DeleteMapping("/")
+    public String DeleteHome(){
+        return "Delete Home";
+    }
+}
---
+++ src/test/java/com/daniilvdovin/SpringTestAPI/SpringTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniilvdovin.SpringTestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
