8ba5e4e719ef50526cb0bf87a46942ef7074334e
Dannil Vdovin <stels1040533@gmail.com>
2023-05-30 11:51:32 +0300

    reg

---
+++ src/main/java/com/daniilvdovin/SpringTestAPI/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.daniilvdovin.SpringTestAPI.controllers;
+
+import com.daniilvdovin.SpringTestAPI.models.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/login")
+    public String PostLogin(@RequestBody User user){
+        return "Hello "+user.name;
+    }
+    @PostMapping("/reg")
+    public String PostReg(@RequestBody User user){
+        return "Hello "+user.name;
+    }
+}
---
+++ src/main/java/com/daniilvdovin/SpringTestAPI/models/User.java
@@ -0,0 +1,9 @@
+package com.daniilvdovin.SpringTestAPI.models;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+public class User {
+    public int ID;
+    public String name;
+}
